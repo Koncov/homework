b3d560a9264a568a437e48de10f8401b9fd521d9
DIAB_notebook <diab@bk.ru>
2024-09-08 14:41:28 +0300

    Домашнее задание 2.76

---
+++ 2.76/main.js
@@ -0,0 +1,21 @@
+// const firstName = 'John'
+// let age = 20
+// let lastName = 'Smith'
+
+// console.log(firstName, lastName, age, 'y.o')
+
+// ==================================================
+//    ================= homework =================
+//        ==================================
+
+
+const someValue = 'Разница между переменными let и const заключается в возможности последующего изменения объявленных с их помощью данных. let - можем изменять, const - не можем. ' 
+
+const myFirstName = 'Koncov'
+const myLastName = 'Evgeniy'
+let myAge = 49
+const myNationality = 'Russia'
+let myCurrentHobby = 'procastination'
+
+console.log(someValue)
+console.log('Фамилия -', myFirstName, 'Имя -', myLastName, 'Возраст -', myAge, 'Гражданство -', myNationality, 'Хобби -', myCurrentHobby)
