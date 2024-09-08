af9823e10031f80c1e79317152dc00922254801f
DIAB_notebook <diab@bk.ru>
2024-09-08 17:12:43 +0300

    Домашнее задание 2.77

---
+++ 2.77/main.js
@@ -0,0 +1,7 @@
+let myInt = 5
+let myFloat = 6.667
+let someInt = 3
+let someFloat = 3.333
+
+console.log(typeof myInt, typeof myFloat)
+console.log(((myFloat + someInt) * (someFloat + myInt)) ** 2)
