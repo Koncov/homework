4b3588de84f75103314d031f2d76881e64f00beb
Evgeniy Koncov <diab@bk.ru>
2024-09-12 15:28:23 +0300

    Домашнее задание 2.87

--- 2.87/main.js
+++ 2.87/main.js
@@ -1,21 +1,28 @@
-const users = [
-  {
-    name: 'alex',
-    age:20
-  },
-  {
-    name: 'den',
-    age: 30
-  },
-]
-
-
-
-const usersName = []
-
-
-users.forEach((user) => {
-  usersName.push(user.name)
+const elements = document.querySelectorAll('div')
+elements.forEach((element) => {
+  element.style.color = 'blue'
 })
 
-console.log(usersName)
+console.log(elements)
+
+const element = document.querySelector('.blockOne')
+
+element.textContent = 'TEXT'
+element.align = 'CENTER'
+element.style.color = 'red'
+element.style.fontSize = '30px'
+
+console.log(element)
+
+const two = document.getElementById('two')
+two.style.background = 'green'
+two.style.display = 'block'
+two.style.maxWidth = '100px'
+two.style.maxHeight = '100px'
+two.style.height = '100%'
+
+three = document.querySelectorAll('.block')
+for (let element of three) {
+  element.style.color = 'grey'
+  element.innerHTML = 'New text'
+}
