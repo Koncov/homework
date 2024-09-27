7b2848f7fbe4c897d4303190bf76523747f7e820
Koncov Evgeniy <diab@bk.ru>
2024-09-27 15:45:42 +0300

    Домашнее задание 2.103

--- 2.103/js/main.js
+++ 2.103/js/main.js
@@ -8,6 +8,7 @@
         const burgerNavLink = e.target.closest('.nav__link')
 
         if (!burgerIcon && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 900) return
 
         if (!document.body.classList.contains('body--opened-menu')) {
             document.body.classList.add('body--opened-menu')
