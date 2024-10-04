3b745650c0786486b85d55f56b49de6763a68084
Koncov Evgeniy <diab@bk.ru>
2024-10-04 16:17:29 +0300

    maket-3

--- maket-3/js/main.js
+++ maket-3/js/main.js
@@ -20,23 +20,23 @@
     }
 
 
-    const modal = document.querySelector('.modal')
-    const modalButton = document.querySelector('.about__img-button')
+        const modal = document.querySelector('.modal')
+        const modalButton = document.querySelector('.about__img-button')
 
-    modalButton.addEventListener('click', openModal)
-    modal.addEventListener('click', closeModal)
+        modalButton.addEventListener('click', openModal)
+        modal.addEventListener('click', closeModal)
 
-    function openModal(e) {
-        e.preventDefault()
-        document.body.classList.toggle('body--opened-modal')
-    }
-
-    function closeModal(e) {
-        e.preventDefault()
-
-        if (e.target.closest('.modal__cancel') || e.target.classList.contains('modal')) {
-            document.body.classList.remove('body--opened-modal')
+        function openModal(e) {
+            e.preventDefault()
+            document.body.classList.toggle('body--opened-modal')
         }
 
-    }
+        function closeModal(e) {
+            e.preventDefault()
+
+            if (e.target.closest('.modal__cancel') || e.target.classList.contains('modal')) {
+                document.body.classList.remove('body--opened-modal')
+            }
+
+        }
 })()
